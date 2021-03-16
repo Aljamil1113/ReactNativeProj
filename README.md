c04cae53659be66e56a0a235cbc2a3846e8e44f8
Aljamil1113 <aarazasaljamil@gmail.com>
2021-03-16 18:31:34 +0800

    Navigation, Search Bar and Styling

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ReactNativeProj
 Explore React Native
+
+# Create New Project
+-npx expo-cli init "projectname" --npm
+
+# install React Navigation
+-npm install react-navigation
+-expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+-npm install react-navigation-stack @react-native-community/masked-view
+-expo r -c
+-npm start
+
+# Icon Listing 
+github.com/expo/vector-icons
---
+++ food/src/components/SearchBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+const SearchBar = ({term, onTermChange, onTermSubmit}) => {
+    return (
+        <View style={styles.backgroundStyle}>
+            <Feather name="search" style={styles.iconStyle}></Feather>
+            <TextInput
+            autoCapitalize="none"
+            autoCorrect={false} 
+            style={styles.inputStyle} 
+            placeholder="Search"
+            value={term}
+            onChangeText={onTermChange}
+            onEndEditing={onTermSubmit}></TextInput>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    backgroundStyle: {
+        backgroundColor: '#F0EEEE',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 15,
+        flexDirection: 'row'
+    },
+    inputStyle: {
+        fontSize: 18,
+        flex: 1
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center'
+    }
+});
+
+export default SearchBar;
