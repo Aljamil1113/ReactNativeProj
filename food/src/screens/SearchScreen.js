2c39be5e916177eed33bd31988b660583fa780bf
Aljamil1113 <aarazasaljamil@gmail.com>
2021-03-17 14:36:19 +0800

    get api values using group and flatlist

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -2,10 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResult from '../hooks/useResult';
+import ResultList from '../components/ResultList';
 
 const SearchScreen = () => {
     const[term, setTerm] = useState('');
     const[searchApi, results, errorMessage] = useResult();
+
+    const filterResultByPrice = (price) => {
+        //price === '$' || '$$' || '$$$'
+        return results.filter(result => {
+            return result.price === price;
+        });
+    };
     
     return (
         <View>
@@ -14,6 +22,9 @@ const SearchScreen = () => {
             onTermSubmit={() => searchApi()}></SearchBar>
             <Text>{errorMessage ?  errorMessage : null}</Text>
             <Text>We have found {results.length}</Text>
+            <ResultList results={filterResultByPrice('$')} title="Best Price"></ResultList>
+            <ResultList results={filterResultByPrice('$$')} title="Bit Pricier"></ResultList>
+            <ResultList results={filterResultByPrice('$$$')} title="Big Spender"></ResultList>
         </View>
     );
 };
