00e04ae504b149dee125bec2d088498d471570ee
Aljamil1113 <aarazasaljamil@gmail.com>
2021-03-17 12:07:02 +0800

    useEffect and sample extract logic

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,33 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResult from '../hooks/useResult';
 
 const SearchScreen = () => {
     const[term, setTerm] = useState('');
-    const[results, setResults] = useState([]);
-    const[errorMessage, setErrorMessage] = useState('');
+    const[searchApi, results, errorMessage] = useResult();
     
-    const searchApi = async (searchTerm) => {
-    try {
-       
-            const response = await yelp.get('/search', {
-                params: {
-                    limit: 50,
-                    term: searchTerm,
-                    location: 'san jose'
-                }
-            });
-        
-            setResults(response.data.businesses);
-        }
-    catch (e) {
-        setErrorMessage('Something went wrong!');
-    }
-};
-
-    
-
     return (
         <View>
             <SearchBar term={term} 
